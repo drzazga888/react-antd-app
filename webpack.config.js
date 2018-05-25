bcf07cd4d240a4047347eb59df79e7b64643b07b
Kamil Drzazga <drzazga888@gmail.com>
2018-05-25 14:04:43 +0200

    Script order

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
-    filename: "./index.html"
+    filename: "./index.html",
+    inject: false
 });
 
 module.exports = {
